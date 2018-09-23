27148cf5e096fa9b022c2cb5261a49ca88832d01
Jokerank <nikitlarkov1997@yandex.ru>
2018-09-23 18:18:47 +0700

    more tweaks and optimizations

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#2 SMP PREEMPT Sun Sep 23 16:55:58 +07 2018"
+#define UTS_VERSION "#3 SMP PREEMPT Sun Sep 23 18:02:31 +07 2018"
 #define LINUX_COMPILE_BY "jokerank"
 #define LINUX_COMPILE_HOST "jokerank-Neptune"
 #define LINUX_COMPILER "gcc version 5.5.0 (Linaro GCC 5.5-2017.10) "
