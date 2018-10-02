d7d695d299e22e059e22854400618f1f2bb1d3f4
Jokerank <nikitalarkov1997@yandex.ru>
2018-10-03 03:26:51 +0700

    clean trash and reduce source size

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 3 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#3 SMP PREEMPT Sun Sep 23 18:02:31 +07 2018"
-#define LINUX_COMPILE_BY "jokerank"
-#define LINUX_COMPILE_HOST "jokerank-Neptune"
-#define LINUX_COMPILER "gcc version 5.5.0 (Linaro GCC 5.5-2017.10) "
